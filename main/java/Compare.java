4ab5b9ac23473e631ba0b52e75f5e94f59f88d2b
Nan Zhang <nan.zhang@morningstar.com>
2019-02-21 12:09:54 -0600

    Error handling when fail to load file

--- main/java/Compare.java
+++ main/java/Compare.java
@@ -19,59 +19,60 @@ public class Compare {
         Scanner input = new Scanner(System.in);
         String firstFile = input.nextLine();
 
-        System.out.println("Pls enter the full path for the 2nd file");
+        System.out.println("Please enter the full path for the 2nd file");
         String secondFile = input.nextLine();
 
         String fileNameA;
         String fileNameB;
         String path;
 
-        HashSet<String> fistFile_Content = loadTxtFile(firstFile);
+        HashSet<String> firstFile_Content = loadTxtFile(firstFile);
         HashSet<String> secondFile_Content = loadTxtFile(secondFile);
 
-        HashSet<String> inFirstButNotSec = new HashSet<String>(fistFile_Content);
-        HashSet<String> inSecButNotFirst = new HashSet<String>(secondFile_Content);
-
-        inFirstButNotSec.removeAll(secondFile_Content);
-        inSecButNotFirst.removeAll(fistFile_Content);
-
-        if (firstFile.lastIndexOf('/') == -1) {
-            // For Windows machine
-            int index = firstFile.lastIndexOf('\\');
-            fileNameA = firstFile.substring(index + 1);
-            fileNameB = secondFile.substring(index + 1);
-            path = firstFile.substring(0, index + 1);
+        if (firstFile_Content.isEmpty() || secondFile_Content.isEmpty()) {
+            System.out.println("Failed to load file content or the file is empty. Please check your path.");
 
         } else {
-            // Linux
-            int index = firstFile.lastIndexOf('/');
-            fileNameA = firstFile.substring(index + 1);
-            fileNameB = secondFile.substring(index + 1);
-            path = firstFile.substring(0, index + 1);
-        }
-        String resultFileNameA = "Items_in_" + fileNameA + "_but_NOT_in_" + fileNameB;
-        String resultFileNameB = "Items_in_" + fileNameB + "_but_NOT_in_" + fileNameA;
+            HashSet<String> inFirstButNotSec = new HashSet<String>(firstFile_Content);
+            HashSet<String> inSecButNotFirst = new HashSet<String>(secondFile_Content);
 
-        StringBuilder inFirstButNotSec_Result = new StringBuilder();
-        StringBuilder inSecButNotFirst_Result = new StringBuilder();
+            inFirstButNotSec.removeAll(secondFile_Content);
+            inSecButNotFirst.removeAll(firstFile_Content);
 
-        for (String s : inFirstButNotSec) {
-            inFirstButNotSec_Result.append(s).append("\n");
-        }
-        for (String s : inSecButNotFirst) {
-            inSecButNotFirst_Result.append(s).append("\n");
-        }
+            if (firstFile.lastIndexOf('/') == -1) {
+                // For Windows machine
+                int index = firstFile.lastIndexOf('\\');
+                fileNameA = firstFile.substring(index + 1);
+                fileNameB = secondFile.substring(index + 1);
+                path = firstFile.substring(0, index + 1);
 
-        try {
-            writeToFile(path + resultFileNameA, inFirstButNotSec_Result.toString());
-            writeToFile(path + resultFileNameB, inSecButNotFirst_Result.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            } else {
+                // Linux
+                int index = firstFile.lastIndexOf('/');
+                fileNameA = firstFile.substring(index + 1);
+                fileNameB = secondFile.substring(index + 1);
+                path = firstFile.substring(0, index + 1);
+            }
+            String resultFileNameA = "Items_in_" + fileNameA + "_but_NOT_in_" + fileNameB;
+            String resultFileNameB = "Items_in_" + fileNameB + "_but_NOT_in_" + fileNameA;
 
-        System.out.println("Result saved to " + resultFileNameA + " and " + resultFileNameB);
-        System.out.println(resultFileNameA + " has " + inFirstButNotSec.size() + " items");
-        System.out.println(resultFileNameB + " has " + inSecButNotFirst.size() + " items");
+            StringBuilder inFirstButNotSec_Result = new StringBuilder();
+            StringBuilder inSecButNotFirst_Result = new StringBuilder();
+
+            inFirstButNotSec.forEach(s -> inFirstButNotSec_Result.append(s).append("\n"));
+            inSecButNotFirst.forEach(s -> inSecButNotFirst_Result.append(s).append("\n"));
+
+            try {
+                writeToFile(path + resultFileNameA, inFirstButNotSec_Result.toString());
+                writeToFile(path + resultFileNameB, inSecButNotFirst_Result.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Result saved to " + resultFileNameA + " and " + resultFileNameB);
+            System.out.println(resultFileNameA + " has " + inFirstButNotSec.size() + " items");
+            System.out.println(resultFileNameB + " has " + inSecButNotFirst.size() + " items");
+        }
     }
 
 
